5593e97080458518492fd789d95363180d27f45d
Siajiza <>
2021-04-18 19:06:46 +0000

    other example with break

--- main.py
+++ main.py
@@ -78,4 +78,19 @@ for username in usernames:
   print(username)
 
 
+# other example with break
+
+def loop_and_break():
+    vegetables = ["onion", "broccoli", "apple", "spinich"]
+    
+    for vegetable in vegetables:
+        if vegetable == 'apple':
+            print(f'{vegetable} is not a vegetable')
+            break
+        print(vegetables)
+        
+    return vegetables
+    
+loop_and_break()
+
 
